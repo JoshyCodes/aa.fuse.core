0e86eaeec94546ea899a7c717d66aadd5e85cde2
Christopher Hahn <chris@creativefuse.org>
2018-06-01 15:25:49 -0400

    Clean webpack config & no new browsersync tab

--- themes/fuse/webpack.config.js
+++ themes/fuse/webpack.config.js
@@ -3,10 +3,10 @@
  *
  * @author  CreativeFuse
  * @since   1.0.0
- * 
- * 
+ *
+ *
  * WP JS Coding Standards Reference: https://make.wordpress.org/core/handbook/best-practices/inline-documentation-standards/javascript/
- * 
+ *
  * Awesome References
  * @Part 1 - https://hackernoon.com/a-tale-of-webpack-4-and-how-to-finally-configure-it-in-the-right-way-4e94c8e7e5c1
  * @Part 2 - https://medium.freecodecamp.org/how-to-develop-react-js-apps-fast-using-webpack-4-3d772db957e4
@@ -53,7 +53,7 @@ const config = {
              *
              * A compiler that allows us to use the latest JS standards without
              * breaking stuff on older browsers.
-             * 
+             *
              * @kind    loader
              * @url     https://github.com/babel/babel-loader
              * @since   1.0.0
@@ -106,7 +106,7 @@ const config = {
                     extract: true,
                     spriteFilename: devMode ? 'sprite.svg' : 'sprite.svg',
                 }
-            }       
+            }
 
 		]
 
@@ -118,10 +118,10 @@ const config = {
 		 * MiniCssExtractPlugin.
          *
          * Create CSS files from our SCSS files and bundle them together.
-		 * 
+		 *
 		 * @url   https://github.com/webpack-contrib/mini-css-extract-plugin
          * @since 1.0.0
-		 * 
+		 *
 		 */
   		new MiniCssExtractPlugin({
 
@@ -133,10 +133,10 @@ const config = {
          * StyleLintPlugin.
          *
          * A Stylelint plugin for webpack.
-         * 
+         *
          * @url   https://github.com/webpack-contrib/stylelint-webpack-plugin
          * @since 1.0.0
-         * 
+         *
          */
         new StyleLintPlugin({
 
@@ -149,11 +149,11 @@ const config = {
 
   		/**
   		 * WebpackMd5Hash
-  		 * 
+  		 *
   		 * @since  1.0.0
-  		 * 
+  		 *
   		 * @repo : https://github.com/erm0l0v/webpack-md5-hash
-  		 * 
+  		 *
   		 * @desc : Plugin to replace a standard webpack chunkhash with md5.
   		 */
         new WebpackMd5Hash(),
@@ -161,7 +161,7 @@ const config = {
         /**
          * BrowserSyncPlugin
          * @since  1.0.0
-         * 
+         *
          * Repo : https://github.com/Va1/browser-sync-webpack-plugin
          * Desc : Implement Browsersync to use live reloading auto style-injecting
          * when assets are compiled. We are not using Webpack Dev Server because it
@@ -180,7 +180,7 @@ const config = {
                     '_dist/*.jpg',
                     '_dist/*.png'
                 ],
-
+                open: false,
                 reloadDelay: 0
             }
         ),
@@ -188,7 +188,7 @@ const config = {
         /**
          * FriendlyErrorsWebpackPlugin
          * @since  1.0.0
-         * 
+         *
          * Repo : https://github.com/geowarin/friendly-errors-webpack-plugin
          * Desc : recognizes certain classes of webpack errors and cleans, aggregates
          * and prioritizes them to provide a better Developer Experience.
@@ -198,20 +198,20 @@ const config = {
         /**
          * CleanWebpackPlugin
          * @since  1.0.0
-         * 
+         *
          * Repo : https://github.com/johnagan/clean-webpack-plugin/
          * Desc : Delete files in specified fodler when a new build is run.
          */
         new CleanWebpackPlugin('_dist', {} ),
 
         /**
-         * CleanObsoleteChunks 
+         * CleanObsoleteChunks
          * @since  1.0.0
-         * 
+         *
          * Repo : https://github.com/GProst/webpack-clean-obsolete-chunks
-         * Desc: Clean old hashed files when new ones are 
+         * Desc: Clean old hashed files when new ones are
          * generated when using --watch.
-         * 
+         *
          * This was implemented because clean-webpack-plugin only
          * works when the initial build is run and would leave
          * all the old files behind when new ones are recompiled
@@ -241,16 +241,16 @@ const config = {
         /**
          * WebpackAssetsManifest
          * @since  1.0.0
-         * 
-         * Repo : https://github.com/webdeveric/webpack-assets-manifest 
+         *
+         * Repo : https://github.com/webdeveric/webpack-assets-manifest
          * Desc : This Webpack plugin will generate a JSON file that matches
          * the original filename with the hashed version. This will help us
          * when it comes time to load our assets.
          */
         new WebpackAssetsManifest({
-            
+
             // Possible SVG Fix https://github.com/kisenka/svg-sprite-loader/issues/166
-           
+
         }),
 
 
@@ -261,7 +261,7 @@ const config = {
 
 	/**
 	 * @since  1.0.0
-	 * 
+	 *
 	 * I like the way Roots/Sage looks when running a build,
 	 * so I grabbed their stats settings
 	 */
