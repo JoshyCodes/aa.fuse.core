568f3d49ef1811808a28b1154ed248ed7d229a1c
Christopher Hahn <chris@creativefuse.org>
2018-06-04 14:13:51 -0400

    Remove Duplicaate Page Title
    If Yoast is present, we will let it generate it’s own.
    
    When we defined it, we were getting title duplicates :/

--- themes/fuse/resources/views/structure/head.php
+++ themes/fuse/resources/views/structure/head.php
@@ -5,7 +5,6 @@
 
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title><?php wp_title('•','true','right'); ?><?php bloginfo('name'); ?></title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
