2087468862b08478580d300d02eeaec74c367dce
Christopher Hahn <chris@creativefuse.org>
2018-06-23 14:51:37 -0400

    Clean Assets Config
    Remove what we no longer need :)

--- themes/fuse/_config/_assets.php
+++ themes/fuse/_config/_assets.php
@@ -19,31 +19,10 @@ return [
 
     ],
 
-    'fonts' => [
-
-    	'google_fonts' => [
-
-			'Lato:300,400,700,900',
-			'Source+Sans+Pro:400,700'
-
-		],
-
-		//No support yet for typekit...but soon!
-		'typekit_id' => '',
-
-    ],
-
     'svg' => [
 
     	'svg_dir'				=> get_theme_file_path() . '/_dist/svg/',
     	'svg_sprite'			=> get_theme_file_path() . '/_dist/svg/sprite.svg',
-
-    ],
-
-    'cachebust' => [
-
-        'core_stylesheet' => file_exists( get_theme_file_path() . '/_dist/css/app.min.css' ) ? filemtime( get_theme_file_path() . '/_dist/css/app.min.css' ) : null,
-        'core_js' => file_exists( get_theme_file_path() . '/_dist/js/app.bundle.js' ) ? filemtime( get_theme_file_path() . '/_dist/js/app.bundle.js' ) : null,
 
     ]
 
