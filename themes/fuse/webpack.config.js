094b199aa13b6ef8ff49fe36a8dabad70b48cb2b
Christopher Hahn <chris@creativefuse.org>
2018-07-10 13:56:26 -0400

    Fic Webpack Bad URL Builds for Fonts

--- themes/fuse/webpack.config.js
+++ themes/fuse/webpack.config.js
@@ -27,288 +27,266 @@ const devMode                       = process.env.NODE_ENV !== 'production';
 const path                          = require( 'path' );
 
 const config = {
-
 	entry: {
-
-		app: 	'./resources/assets/js/app.js',
-		critical: 	'./resources/assets/js/critical.js',
-		posts: './resources/assets/js/posts.js',
-
+		app: "./resources/assets/js/app.js",
+		critical: "./resources/assets/js/critical.js",
+		posts: "./resources/assets/js/posts.js"
 	},
 
-	devtool: devMode ? 'none' : 'none',
+	devtool: devMode ? "none" : "none",
 
 	output: {
+		path: path.resolve(__dirname, "_dist"),
 
-		path: path.resolve( __dirname, '_dist' ),
-
-		filename: devMode  ? 'js/[name].js' : 'js/[name].[chunkhash].bundle.js',
-		chunkFilename: devMode ? 'js/[name].js' : 'js/[name].[chunkhash].js',
-		sourceMapFilename: '[file].map'
-
+		filename: devMode ? "js/[name].js" : "js/[name].[chunkhash].bundle.js",
+		chunkFilename: devMode ? "js/[name].js" : "js/[name].[chunkhash].js",
+		sourceMapFilename: "[file].map"
 	},
 
 	module: {
-
 		rules: [
-
 			/**
 			 * Babel Loader.
-             *
-             * A compiler that allows us to use the latest JS standards without
-             * breaking stuff on older browsers.
-             *
-             * @kind    loader
-             * @url     https://github.com/babel/babel-loader
-             * @since   1.0.0
+			 *
+			 * A compiler that allows us to use the latest JS standards without
+			 * breaking stuff on older browsers.
+			 *
+			 * @kind    loader
+			 * @url     https://github.com/babel/babel-loader
+			 * @since   1.0.0
 			 */
-            {
-                test:   /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: ['babel-loader', 'eslint-loader']
-            },
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: ["babel-loader", "eslint-loader"]
+			},
 
 			/**
 			 * SCSS Loader w/ POSTCss
-             *
-             * @kind    loader
-             * @since   1.0.0
-             * @uses    MiniCssExtractPlugin
+			 *
+			 * @kind    loader
+			 * @since   1.0.0
+			 * @uses    MiniCssExtractPlugin
 			 */
 			{
-
 				test: /\.scss$/,
-	            use: [
-	                'style-loader',
-                	ExtractCssChunks.loader,
-                	{
-                		loader: 'css-loader', options: { sourceMap: true }
-                	},
-                	'postcss-loader',
+				use: [
+					"style-loader",
+					ExtractCssChunks.loader,
+					{
+						loader: "css-loader",
+						options: { sourceMap: true }
+					},
+					"postcss-loader",
 
-                	{
-                		loader: 'sass-loader', options: { sourceMap: true }
-                	}
-	            ],
-
+					{
+						loader: "sass-loader",
+						options: { sourceMap: true }
+					}
+				]
 			},
 
 			/**
 			 * Font Loader
 			 */
 			{
-	            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-	            use: [
-	            	{
-	                	loader: 'file-loader',
-	                	options: {
-	                    	name: '[name].[ext]',
-	                    	outputPath: 'fonts/',
-	                    	publicPath: '../wp-content/themes/fuse/_dist/fonts/'
-	                	}
-	            	}
-	            ]
-	        },
-
-            /**
-             * SVG Sprite Loader.
-             *
-             * A loader to handle managing our SVG sprites.
-             *
-             * @kind    loader
-             * @url     https://github.com/kisenka/svg-sprite-loader
-             * @url		https://github.com/rpominov/svgo-loader
-             * @url 	https://github.com/svg/svgo
-             * @since   1.0.0
-             */
-
-            {
-             	test: /\.svg$/,
-             	use: [{
-
-					loader: 'svg-sprite-loader', options: {
-	                    extract: true,
-	                    spriteFilename: devMode ? 'sprite.svg' : 'sprite.svg',
-	                }
-
-	            },
-
-				{
-					loader: 'svgo-loader', options: {
-
-						plugins: [
-							{cleanupAttrs: true},
-							{removeTitle: true},
-							{removeUselessStrokeAndFill: true},
-	                        {convertColors: {shorthex: false}},
-	                        {convertShapeToPath: true},
-	                        {removeAttrs: {attrs: '(stroke|fill)'}},
-	                        {removeMetadata: true}
-						]
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "fonts/",
+							publicPath:"/wp-content/themes/fuse/_dist/fonts/"
+						}
 					}
-
-				}
 				]
-            }
+			},
 
+			/**
+			 * SVG Sprite Loader.
+			 *
+			 * A loader to handle managing our SVG sprites.
+			 *
+			 * @kind    loader
+			 * @url     https://github.com/kisenka/svg-sprite-loader
+			 * @url		https://github.com/rpominov/svgo-loader
+			 * @url 	https://github.com/svg/svgo
+			 * @since   1.0.0
+			 */
+
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: "svg-sprite-loader",
+						options: {
+							extract: true,
+							spriteFilename: devMode
+								? "sprite.svg"
+								: "sprite.svg"
+						}
+					},
+
+					{
+						loader: "svgo-loader",
+						options: {
+							plugins: [
+								{ cleanupAttrs: true },
+								{ removeTitle: true },
+								{ removeUselessStrokeAndFill: true },
+								{ convertColors: { shorthex: false } },
+								{ convertShapeToPath: true },
+								{ removeAttrs: { attrs: "(stroke|fill)" } },
+								{ removeMetadata: true }
+							]
+						}
+					}
+				]
+			}
 		]
-
 	},
 
 	plugins: [
-
 		/**
 		 * MiniCssExtractPlugin.
-         *
-         * Create CSS files from our SCSS files and bundle them together.
+		 *
+		 * Create CSS files from our SCSS files and bundle them together.
 		 *
 		 * @url   https://github.com/webpack-contrib/mini-css-extract-plugin
-         * @since 1.0.0
+		 * @since 1.0.0
 		 *
 		 // */
 
-        new ExtractCssChunks({
-
+		new ExtractCssChunks({
 			// Options similar to the same options in webpackOptions.output
 			// both options are optional
-			filename: devMode ? 'css/[name].css' : 'css/[name].[contenthash].bundle.css',
-			chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[contenthash].bundle.css',
+			filename: devMode
+				? "css/[name].css"
+				: "css/[name].[contenthash].bundle.css",
+			chunkFilename: devMode
+				? "css/[id].css"
+				: "css/[id].[contenthash].bundle.css",
 			hot: true // optional is the plguin cannot automatically detect if you are using HOT, not for production use
+		}),
 
-        }),
+		/**
+		 * StyleLintPlugin.
+		 *
+		 * A Stylelint plugin for webpack.
+		 *
+		 * @url   https://github.com/webpack-contrib/stylelint-webpack-plugin
+		 * @since 1.0.0
+		 *
+		 */
+		new StyleLintPlugin({
+			configFile: ".stylelintrc",
+			failOnError: false,
+			files: "**/*.scss",
+			quiet: false
+		}),
 
-        /**
-         * StyleLintPlugin.
-         *
-         * A Stylelint plugin for webpack.
-         *
-         * @url   https://github.com/webpack-contrib/stylelint-webpack-plugin
-         * @since 1.0.0
-         *
-         */
-        new StyleLintPlugin({
+		/**
+		 * WebpackMd5Hash
+		 *
+		 * @since  1.0.0
+		 *
+		 * @repo : https://github.com/erm0l0v/webpack-md5-hash
+		 *
+		 * @desc : Plugin to replace a standard webpack chunkhash with md5.
+		 */
+		new WebpackMd5Hash(),
 
-            configFile: '.stylelintrc',
-            failOnError: false,
-            files: '**/*.scss',
-            quiet: false,
+		/**
+		 * BrowserSyncPlugin
+		 * @since  1.0.0
+		 *
+		 * Repo : https://github.com/Va1/browser-sync-webpack-plugin
+		 * Desc : Implement Browsersync to use live reloading auto style-injecting
+		 * when assets are compiled. We are not using Webpack Dev Server because it
+		 * is a pain in the ass to configure with Wordpress. Unfortunately this means
+		 * we can not use Hot Module Replacement though :(
+		 */
+		new BrowserSyncPlugin({
+			injectCss: true,
+			proxy: "https://cf.dev",
 
-        }),
+			files: [
+				"**/*.php",
+				"_dist/*.js",
+				"_dist/*.svg",
+				"_dist/*.jpg",
+				"_dist/*.png"
+			],
 
-  		/**
-  		 * WebpackMd5Hash
-  		 *
-  		 * @since  1.0.0
-  		 *
-  		 * @repo : https://github.com/erm0l0v/webpack-md5-hash
-  		 *
-  		 * @desc : Plugin to replace a standard webpack chunkhash with md5.
-  		 */
-        new WebpackMd5Hash(),
+			open: false,
+			reloadDelay: 0
+		}),
 
-        /**
-         * BrowserSyncPlugin
-         * @since  1.0.0
-         *
-         * Repo : https://github.com/Va1/browser-sync-webpack-plugin
-         * Desc : Implement Browsersync to use live reloading auto style-injecting
-         * when assets are compiled. We are not using Webpack Dev Server because it
-         * is a pain in the ass to configure with Wordpress. Unfortunately this means
-         * we can not use Hot Module Replacement though :(
-         */
-    	new BrowserSyncPlugin( {
+		/**
+		 * FriendlyErrorsWebpackPlugin
+		 * @since  1.0.0
+		 *
+		 * Repo : https://github.com/geowarin/friendly-errors-webpack-plugin
+		 * Desc : recognizes certain classes of webpack errors and cleans, aggregates
+		 * and prioritizes them to provide a better Developer Experience.
+		 */
+		new FriendlyErrorsWebpackPlugin({}),
 
-                injectCss: true,
-                proxy: 'https://cf.dev',
+		/**
+		 * CleanWebpackPlugin
+		 * @since  1.0.0
+		 *
+		 * Repo : https://github.com/johnagan/clean-webpack-plugin/
+		 * Desc : Delete files in specified fodler when a new build is run.
+		 */
+		new CleanWebpackPlugin("_dist", {}),
 
-                files: [
-                    '**/*.php',
-                    '_dist/*.js',
-                    '_dist/*.svg',
-                    '_dist/*.jpg',
-                    '_dist/*.png'
-                ],
+		/**
+		 * CleanObsoleteChunks
+		 * @since  1.0.0
+		 *
+		 * Repo : https://github.com/GProst/webpack-clean-obsolete-chunks
+		 * Desc: Clean old hashed files when new ones are
+		 * generated when using --watch.
+		 *
+		 * This was implemented because clean-webpack-plugin only
+		 * works when the initial build is run and would leave
+		 * all the old files behind when new ones are recompiled
+		 * with new hashes.
+		 */
+		new CleanObsoleteChunks({}),
 
-                open: false,
-                reloadDelay: 0
-            }
-        ),
+		new CopyWebpackPlugin([
+			// Image Handler + add content hash
+			{
+				from: "./resources/assets/images/**/*",
+				to: "./images/",
+				flatten: true
+			},
 
-        /**
-         * FriendlyErrorsWebpackPlugin
-         * @since  1.0.0
-         *
-         * Repo : https://github.com/geowarin/friendly-errors-webpack-plugin
-         * Desc : recognizes certain classes of webpack errors and cleans, aggregates
-         * and prioritizes them to provide a better Developer Experience.
-         */
-        new FriendlyErrorsWebpackPlugin({
+			// Copy fonts to dist
+			{
+				from: "./resources/assets/fonts/**/*",
+				to: "./fonts/",
+				flatten: true
+			}
+		]),
 
+		/**
+		 * WebpackAssetsManifest
+		 * @since  1.0.0
+		 *
+		 * Repo : https://github.com/webdeveric/webpack-assets-manifest
+		 * Desc : This Webpack plugin will generate a JSON file that matches
+		 * the original filename with the hashed version. This will help us
+		 * when it comes time to load our assets.
+		 */
+		new WebpackAssetsManifest({
+			// Possible SVG Fix https://github.com/kisenka/svg-sprite-loader/issues/166
+		}),
 
-        }),
-
-        /**
-         * CleanWebpackPlugin
-         * @since  1.0.0
-         *
-         * Repo : https://github.com/johnagan/clean-webpack-plugin/
-         * Desc : Delete files in specified fodler when a new build is run.
-         */
-        new CleanWebpackPlugin('_dist', {} ),
-
-        /**
-         * CleanObsoleteChunks
-         * @since  1.0.0
-         *
-         * Repo : https://github.com/GProst/webpack-clean-obsolete-chunks
-         * Desc: Clean old hashed files when new ones are
-         * generated when using --watch.
-         *
-         * This was implemented because clean-webpack-plugin only
-         * works when the initial build is run and would leave
-         * all the old files behind when new ones are recompiled
-         * with new hashes.
-         */
-        new CleanObsoleteChunks({}),
-
-
-        new CopyWebpackPlugin([
-
-            // Image Handler + add content hash
-            {
-                from:   './resources/assets/images/**/*',
-                to:     './images/',
-                flatten: true
-            },
-
-            // Copy fonts to dist
-            {
-                from: './resources/assets/fonts/**/*',
-                to: './fonts/',
-                flatten: true
-            },
-
-        ]),
-
-        /**
-         * WebpackAssetsManifest
-         * @since  1.0.0
-         *
-         * Repo : https://github.com/webdeveric/webpack-assets-manifest
-         * Desc : This Webpack plugin will generate a JSON file that matches
-         * the original filename with the hashed version. This will help us
-         * when it comes time to load our assets.
-         */
-        new WebpackAssetsManifest({
-
-            // Possible SVG Fix https://github.com/kisenka/svg-sprite-loader/issues/166
-
-        }),
-
-
-        new SpriteLoaderPlugin({ plainSprite: true })
-
-
+		new SpriteLoaderPlugin({ plainSprite: true })
 	],
 
 	/**
@@ -318,20 +296,19 @@ const config = {
 	 * so I grabbed their stats settings
 	 */
 	stats: {
-	  hash: false,
-	  version: false,
-	  timings: false,
-	  children: false,
-	  errors: false,
-	  errorDetails: false,
-	  warnings: false,
-	  chunks: false,
-	  modules: false,
-	  reasons: false,
-	  source: false,
-	  publicPath: false,
+		hash: false,
+		version: false,
+		timings: false,
+		children: false,
+		errors: false,
+		errorDetails: false,
+		warnings: false,
+		chunks: false,
+		modules: false,
+		reasons: false,
+		source: false,
+		publicPath: false
 	}
-
 };
 
 module.exports = config;
